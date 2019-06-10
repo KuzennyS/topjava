20372f18b085fd82582ee044fe0e3397b32e356b
Sergey Kuzenny <kuzenny4@gmail.com>
2019-06-10 15:04:06 +0300

    1 hw1.2

---
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -0,0 +1,57 @@
+package ru.javawebinar.topjava.model;
+
+import java.time.LocalDateTime;
+
+public class MealTo {
+    private final LocalDateTime dateTime;
+
+    private final String description;
+
+    private final int calories;
+
+    private final boolean excess;
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess, int id) {
+        this.id = id;
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+        this.excess = excess;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public boolean isExcess() {
+        return excess;
+    }
+
+    @Override
+    public String toString() {
+        return "MealTo{" +
+                "dateTime=" + dateTime +
+                ", description='" + description + '\'' +
+                ", calories=" + calories +
+                ", excess=" + excess +
+                '}';
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,27 @@
+<%--<html>--%>
+<%--<head>--%>
+<%--    <meta charset="UTF-8">--%>
+<%--    <title>Java Enterprise (Topjava)</title>--%>
+<%--</head>--%>
+<%--<body>--%>
+<%--<h3>¬±¬â¬à¬Ö¬Ü¬ä <a href="https://github.com/JavaWebinar/topjava" target="_blank">Java Enterprise (Topjava)</a></h3>--%>
+
+<%--<hr>--%>
+<%--<ul>--%>
+<%--    <li><a href="users">Users</a></li>--%>
+<%--    <li><a href="meals">Meals</a></li>--%>
+<%--</ul>--%>
+
+<%--</body>--%>
+<%--</html>--%>
+<%@ page language="java" contentType="text/html; charset=EUC-KR" pageEncoding="EUC-KR"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+    <title>Insert title here</title>
+</head>
+<body>
+<jsp:forward page="/meals?action=listMeal" />
+</body>
+</html>
