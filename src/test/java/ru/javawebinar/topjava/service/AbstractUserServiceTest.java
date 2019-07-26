94122b57322ef1d8abeb1a3ca2be3e408d36663c
Sergey Kuzenny <kuzenny4@gmail.com>
2019-07-26 19:02:36 +0300

    7 03 HW6 fix relative url utf8

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.service;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
